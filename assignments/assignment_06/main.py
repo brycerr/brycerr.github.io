d52601f8d775d351f0c5b8995b782c4916100c93
brycerr <94137678+brycerr@users.noreply.github.com>
2025-05-12 18:05:29 -0500

    Proportion of Variance

--- assignments/assignment_06/main.py
+++ assignments/assignment_06/main.py
@@ -49,7 +49,6 @@ def center_data(data):
 
 
 def my_pca(data):
-    # TODO: implement pca
     # (can use the built-in functions to compute eigenvalues and eigen vectors)
 
     # create covariance matrix
@@ -57,13 +56,12 @@ def my_pca(data):
     cov_matrix = np.dot(matrix_x.T, matrix_x) / (matrix_x.shape[0] - 1)
 
     # eigen decomposition
-    w, v = np.linalg.eigh(cov_matrix)    # w: eigenvalues, v: eigenvectors
+    w, v = np.linalg.eigh(cov_matrix)       # w: eigenvalues, v: eigenvectors
     # print(w.tolist())
     # print(v.tolist())
 
     # sort the two lists
-    sorted_indices = np.argsort(w)[::-1]    # sorts the indices of w in reverse order
-
+    sorted_indices = np.argsort(w)[::-1]    # sort the indices of w in reverse order
     latent = []     # sorted eigen values in descending order
     coeff = []      # corresponding sorted eigen vector
     for i in sorted_indices:
@@ -78,39 +76,68 @@ def my_pca(data):
 def eigen_faces(data, num_faces, x, y):
     coeff, latent = my_pca(data)
 
-    # TODO: display the first given eigen faces using pyplot
-    #   - customizable in main function
-    #   - example: plt.imshow(eigen_face_image, cmap='gray')
-
     # convert the lists of eigen faces into 30 * 32 images
     for i in range(num_faces):
         eigen_face = coeff[i]
         eigen_face_image = []
         for j in range(x):
-            temp = eigen_face[j*y:(j+1)*32]
+            temp = eigen_face[j*y:(j+1)*y]
             eigen_face_image.append(temp)
 
         # display image
         plt.imshow(eigen_face_image, cmap='gray')
+        plt.title(f"Eigenface {i + 1}")
         plt.show()
 
 
-def proportion_of_variance(data):
-    # TODO: c1
-    # TODO: c2
-    # TODO: c3
-    # TODO: c4
-    return  # number of eigen vectors that can explain 90% of cariance
+def proportion_of_variance(data, var=0.90):
+    # c2
+    coeff, latent = my_pca(data)
+    w_total = 0     # sum of all eigenvalues
+    for i in range(len(latent)):
+        w_total += latent[i]
+    # print(f"w_total: {w_total}")
 
+    pov = [0 for _ in range(len(latent))]
+    for i in range(len(latent)):
+        pov[i] = latent[i] / w_total
 
-# TODO:
-#   c
-#   d
-#   e
+    # calculate cumulative sum of elements
+    cum_sum_pov = []
+    current = 0
+    for i in range(len(pov)):
+        current += pov[i]
+        cum_sum_pov.append(current)
+    # print(f"Cumulative Sum: {cum_sum_pov}")
+
+    # c3
+    # find index k of the first principal component for which the cumulative sum of elements exceeds 90
+    k = -1
+    for i in range(len(cum_sum_pov)):
+        if cum_sum_pov[i] >= var:
+            k = i
+            break
+    # print(f"k: {k}")
+    if k == -1:
+        print(f"Something went very wrong. No k is greater than {var}.")
+
+    # c4
+    # plot the cumulative function
+    plt.title("Cumulative Function")
+    plt.plot([x for x in range(len(cum_sum_pov))], cum_sum_pov)
+    plt.axhline(y=var, color='red', linestyle='--', label=f"Variance threshold")
+    plt.plot(k, cum_sum_pov[k], marker='o', color='red', linestyle='', label=f"k = {k} Principal Components")
+    plt.grid(True)
+    plt.xlabel("Number of Principal Components")
+    plt.ylabel("Cumulative Proportion of Variance")
+    plt.legend()
+    plt.show()
+
+    return k
 
 
 def main():
-    num_faces = 10
+    num_faces = 0
     face_x = 30
     face_y = 32
 
@@ -122,6 +149,16 @@ def main():
     train_data = center_data(raw_train_data)
 
     eigen_faces(train_data, num_faces, face_x, face_y)
+
+    # c
+    k = proportion_of_variance(train_data)
+    print(f"k: {k}")
+
+    # TODO: d
+    # Project the training data and test data to the K principle components that we find in section c for K={1,3,5,7}.
+
+    # TODO: e
+    # Use the first k components (k= {50, 100}) to approximate the first five images in the training set
 
 
 if __name__ == "__main__":
